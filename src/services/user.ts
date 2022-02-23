2bf6f2903226cd21dc442eae6caa640aade30152
Felipe Martins <mfelipeluiz99@gmail.com>
2022-02-23 18:49:05 -0300

    Req01: impl service for Users

---
+++ src/services/user.ts
@@ -0,0 +1,26 @@
+import jwt from 'jsonwebtoken';
+import dotenv from 'dotenv';
+import { IUser } from '../models/IUser';
+
+import models from '../models/User';
+
+dotenv.config();
+
+const SECRET: string = process.env.JWT_SECRET || 'felps';
+
+interface AUTH {
+  token: string;
+}
+
+const createUser = async (data: IUser): Promise<AUTH> => {
+  await models.create(data);
+
+  const token = jwt.sign({ data }, SECRET, {
+    expiresIn: '7d',
+    algorithm: 'HS256',
+  });
+  
+  return { token };
+};
+
+export default createUser;
