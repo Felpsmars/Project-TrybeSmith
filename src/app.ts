bb328443c917d94949e713c8f22f608b247fb2a1
Felipe Martins <mfelipeluiz99@gmail.com>
2022-02-24 19:31:38 -0300

    Req03: add route on app

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import userRoutes from './routes/user.routes';
 import loginRoutes from './routes/login.routes';
+import productRoutes from './routes/products.routes';
 
 const app = express();
 
@@ -8,5 +9,6 @@ app.use(express.json());
 
 app.use('/users', userRoutes);
 app.use('/login', loginRoutes);
+app.use('/products', productRoutes);
 
 export default app;
