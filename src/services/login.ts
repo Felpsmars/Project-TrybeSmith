c09f4ce5af652b99b0902425a7628ea994abeced
Felipe Martins <mfelipeluiz99@gmail.com>
2022-02-24 16:50:26 -0300

    Req02: login service

---
+++ src/services/login.ts
@@ -0,0 +1,15 @@
+import dotenv from 'dotenv';
+import { Login } from '../models/ILogin';
+
+import models from '../models/Login';
+
+dotenv.config();
+
+const createUser = async (data: Login) => {
+  const { username, password } = data;
+  const loginData = await models.loginModel({ username, password });
+  
+  return loginData;
+};
+
+export default createUser;
